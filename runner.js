7c657a5b53dcc6dfa97347d391318f394a5548f6
ichemlal <74398162+ichemlal@users.noreply.github.com>
2023-01-15 21:12:36 +0100

    Update runner.js

--- runner.js
+++ runner.js
@@ -2,8 +2,7 @@
   'use strict'
 
   var pm2 = require('pm2')
-  var path = require('path')
-  var nconf = require('nconf')
+
 
   pm2.connect(true, function (err) {
     if (err) throw err
@@ -11,9 +10,9 @@
     pm2.start(
       {
         name: 'trudesk-'+process.argv[3],
-        script: path.join(process.argv[4], '/app.js'),
-        output: path.join(process.argv[4], '/logs/output.log'),
-        error: path.join(process.argv[4], '/logs/output.log'),
+  script: process.argv[4]+'/app.js',
+        output:process.argv[4]+'/logs/output.log',
+        error: process.argv[4]+'/logs/output.log',
         mergeLogs: true,
         env:{
           PORT:process.argv[2],
@@ -26,7 +25,7 @@
           throw err
         }
         console.log(process.argv[2]);
-         nconf.set('dbt',process.argv[3]);
+        
         console.log(__dirname);
       console.log(process.argv[4]);
 
